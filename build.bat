00cd9d2c684d870515bcb0a23e14c05ad13e149c
agavrin2000 <anton.gavrin@gmail.com>
2020-04-21 12:12:29 +0300

    update version, add archive commands

---
+++ build.bat
@@ -0,0 +1,5 @@
+call npm install
+call npm run-script build
+tar -cvzf custom_components\fontawesome\data\fas.js.gz custom_components\fontawesome\data\fas.js
+tar -cvzf custom_components\fontawesome\data\far.js.gz custom_components\fontawesome\data\far.js
+tar -cvzf custom_components\fontawesome\data\fab.js.gz custom_components\fontawesome\data\fab.js
